7168286bb1e9221642dfe3b7865388ccde9672ef
Joe Whittles <joewhittles@gmail.com>
2021-04-16 17:27:51 +0100

    Add failing bidirectional test

--- lib/core-types-to-ts.spec.ts
+++ lib/core-types-to-ts.spec.ts
@@ -1,5 +1,6 @@
 import { NamedType, NodeDocument } from 'core-types';
 import { convertCoreTypesToTypeScript } from './core-types-to-ts'
+import { convertTypeScriptToCoreTypes } from './ts-to-core-types';
 
 
 const wrapDocument = ( types: Array< NamedType > ): NodeDocument =>
@@ -238,6 +239,34 @@ describe( "core-types-to-ts", ( ) =>
 
 		expect( ts ).toMatchSnapshot( );
 	} );
+
+  it( "should be bidirectional", ( ) => {
+    const ts = convertCoreTypesToTypeScript(
+      wrapDocument([
+        {
+          name: "User",
+          title: "User type",
+          description: "This type holds the user information, such as name",
+          type: "object",
+          properties: {
+            name: {
+              node: { type: "string", title: "The real name" },
+              required: true,
+            },
+          },
+          additionalProperties: false,
+        },
+      ]),
+      { noDescriptiveHeader: true }
+    );
+
+    const epi = convertCoreTypesToTypeScript(
+      convertTypeScriptToCoreTypes(ts.data).data,
+      { noDescriptiveHeader: true }
+    );
+
+    expect(epi).toEqual(ts);
+  });
 
 	it( "should not add descriptive header", ( ) =>
 	{
